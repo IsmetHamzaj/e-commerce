cfc0516bdc28474e9d7cb198baed04fa898dbf0d
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-04-23 04:22:37 +0200

    Add files via upload

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import './../footer.css'
-import { FaLinkedinIn, FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FaLinkedinIn, FaFacebookF, FaInstagram, FaTwitter, FaArrowRight } from 'react-icons/fa'
 function Footer() {
     return (
         <div>
-            <div className='footer'>
-                <div>
-                    <div className='terms'>
-                        <p>CONTACT</p>
-                        <p>TERMS OF SERVICES</p>
-                        <p>SHIPPING AND RETURNS</p>
+            <div className='footer-container'>
+                <div className='footer'>
+                    <div className='footer-heading footer-1'>
+                        <div className='terms'>
+                            <p>CONTACT</p>
+                            <p>TERMS OF SERVICES</p>
+                            <p>SHIPPING AND RETURNS</p>
+                        </div>
+                        <p>&copy; <b>2021 Shelly.</b> Terms use <b>and</b> privacy policy</p>
                     </div>
-                    <div>
-                        <p className='copyright'>&copy; <b>2021 Shelly</b>.Terms of use <b>and</b> privacy policy</p>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <input className='email' type="email" placeholder='Give an email, get the newsletter' />
-                    </div>
-                    <div>
-                        <FaLinkedinIn />
-                        <FaFacebookF />
-                        <FaInstagram />
-                        <FaTwitter />
+                    <div className='footer-heading footer-2'>
+                        <div className='feedback'>
+                            <input className='email' type="email" placeholder='Give an email, get the newsletter' />
+                            <FaArrowRight className='arrow' />
+                        </div>
+                        <div className='icon'>
+                            <div>
+                                <FaLinkedinIn />
+                            </div>
+                            <div>
+                                <FaFacebookF />
+                            </div>
+                            <div>
+                                <FaInstagram />
+                            </div>
+                            <div>
+                                <FaTwitter />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
