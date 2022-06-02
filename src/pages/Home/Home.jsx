ee95c2ee02744b2ee56e20463bf6808bc18255a9
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-06-03 00:09:28 +0100

    Add files via upload

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import LayOut from '../../layouts/LayoutFile'
-import auth from '../../auth'
+// import auth from '../../auth'
 const Home = () => {
     return(
         <div>
             <h1>Log In</h1>
-            <button onClick={
+            {/* <button onClick={
                 () => {
                     auth.login(() => {
                         this.props.history.push("/my-account")
                     })
                 }
-            }>Login</button>
+            }>Login</button> */}
         </div>
     )
 }
--- src/pages/MyAcc/MyAcc.jsx
+++ src/pages/MyAcc/MyAcc.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 // import { useAuth } from '../../auth'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Navigate } from 'react-router'
 import LayOut from '../../layouts/LayoutFile'
 import LogIn from '../LogIn'
-const MyAccount = ({user}) => {
-    if(!user) {
-        return <Navigate to="/login" replace />
-    }
+const MyAccount = ({logout}) => {
+    return(
+        <div>
+            <Link to="/my-account">My Account</Link>
+            <h1>
+                Hi you are logged in
+            </h1>
+            <button onClick={logout}>LogOut</button>
+        </div>
+    )
+    // if(!user) {
+    //     return <Navigate to="/login" replace />
+    // }
     // // const auth = useAuth()
     // const navigate = useNavigate()
     // const handleLogout = () => {
