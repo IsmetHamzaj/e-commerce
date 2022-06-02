3f37215fb0f814c3011c036db703b771b0692553
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-06-03 00:00:56 +0100

    Add files via upload

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,10 +1,24 @@
-import {React, useState} from 'react'
+import { React, useState } from 'react'
 import { useAuth } from '../../auth'
 import { useNavigate } from 'react-router-dom'
 import auth from '../../auth'
 import LayOut from '../../layouts/LayoutFile'
 const LogIn = () => {
-    
+    const [user, setUser] = React.useState(null);
+
+    const handleLogin = () => setUser({ id: '1', name: 'robin' });
+    const handleLogout = () => setUser(null);
+    return (
+        <>
+            {
+                user ? (
+                    <button onClick={handleLogout} > Sign Out</button>
+                ) : (
+                    <button onClick={handleLogin}>Sign In</button>
+                )
+            }
+        </>
+    )
     // const [user, setUser] = useState("")
     // const auth = useAuth()
     // const navigate = useNavigate()
--- src/pages/MyAcc/MyAcc.jsx
+++ src/pages/MyAcc/MyAcc.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 // import { useAuth } from '../../auth'
 import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router'
 import LayOut from '../../layouts/LayoutFile'
-const MyAccount = () => {
-    // const auth = useAuth()
-    const navigate = useNavigate()
-    const handleLogout = () => {
-        // auth.logout()
-        navigate('/my-account')
+import LogIn from '../LogIn'
+const MyAccount = ({user}) => {
+    if(!user) {
+        return <Navigate to="/login" replace />
     }
-    return (
-        <div>
-            {/* Welcome {auth.user} */}
-            <button onClick={handleLogout}>Log Out</button>
-        </div>
-    )
+    // // const auth = useAuth()
+    // const navigate = useNavigate()
+    // const handleLogout = () => {
+    //     // auth.logout()
+    //     navigate('/my-account')
+    // }
+    // return (
+    //     <div>
+    //         {/* Welcome {auth.user} */}
+    //         <button onClick={handleLogout}>Log Out</button>
+    //     </div>
+    // )
+
 }
 
 export default LayOut(MyAccount)
