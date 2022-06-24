e724aca162def6ef64cc9b1705f45d170c5ce388
Ismet Hamzaj <81379437+IsmetHamzaj@users.noreply.github.com>
2022-06-24 02:24:12 +0200

    Creating login and register form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import styles from './Footer.module.scss'
+import { FaLinkedinIn, FaFacebookF, FaInstagram, FaTwitter, FaArrowRight } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+            <div className={styles.footer_container}>
+                <div className={styles.footer}>
+                    <div className={styles.footer_heading}>
+                        <div className={styles.terms}>
+                            <p>CONTACT</p>
+                            <p>TERMS OF SERVICES</p>
+                            <p>SHIPPING AND RETURNS</p>
+                        </div>
+                        <p>&copy; <b>2021 Shelly.</b> Terms use <b>and</b> privacy policy</p>
+                    </div>
+                    <div className={styles.footer_heading}>
+                        <div className={styles.feedback}>
+                            <input className={styles.email} type="email" placeholder='Give an email, get the newsletter' />
+                            <FaArrowRight className={styles.arrow} />
+                        </div>
+                        <div className={styles.icon}>
+                            <div>
+                                <FaLinkedinIn />
+                            </div>
+                            <div>
+                                <FaFacebookF />
+                            </div>
+                            <div>
+                                <FaInstagram />
+                            </div>
+                            <div>
+                                <FaTwitter />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/layouts/LayoutFile.jsx
+++ src/layouts/LayoutFile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from './../components/Header'
+import Footer from "../components/Footer";
 
 const LayOut = (BaseComponent) => (props) => {
     return (
         <div>
             <Header />
             <BaseComponent {...props} />
+            <Footer />
         </div>
     )
 }
